fcf8fa5e8cd16a3f7c5c6cfc49bcbaebe9f76af2
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-02 00:09:53 -0700

    Import configStore

--- frontend/friends_flix.jsx
+++ frontend/friends_flix.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './store/store.js'
+import {signup, signin, signout} from './actions/session_actions'
+
 // add DOMContentLoaded Listener to render, when html finish loading
 document.addEventListener("DOMContentLoaded", ()=>{
     //load success
 
+    const store = configureStore();
     const rootElemenet = document.getElementById('root');
 
     //render content to dom
     ReactDOM.render(<h1>Welcome to FriendsFlix</h1>, rootElemenet);
+
+    // //testing
+    // window.store = store;
+    // window.signin = signin;
+    // window.signout = signout;
+    // window.signup = signup;
 });
