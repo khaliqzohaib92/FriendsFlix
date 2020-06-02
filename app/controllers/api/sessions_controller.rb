30e11b1c1568481577d2f22be139e74a98bb5c5b
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-01 22:20:22 -0700

    Fix bugs

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::SessionsController < ApplicationController
     before_action :ensure_signed_in, only: [:destroy]
 
     def create
-        @user = User.find_by_credentails(user_params[:email], user_params[:password])
+        @user = User.find_by_credentials(user_params[:email], user_params[:password])
 
         if @user
             signin(@user)
@@ -13,13 +13,8 @@ class Api::SessionsController < ApplicationController
     end
 
     def destroy
-        @user = User.find(params[:id])
-        if @user.destroy
-            signout
-            my_render(200, {})
-        else
-            my_render(422, @user.errors.full_messages)
-        end
+        signout
+        my_render(200, {message: "Logged out!"})
     end
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
-    resources :session, only: [:create, :delete]
+    resource :session, only: [:create, :destroy]
   end
 end
