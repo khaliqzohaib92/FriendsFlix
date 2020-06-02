6795e4145381204a9042de61e8f002cf264e826c
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-01 21:09:53 -0700

    fix bugs in user controller and model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,9 +2,10 @@ class Api::UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        debugger
         if @user.save
             signin(@user)
-            render :signin
+            render :signup
         else
             my_render(422, @user.errors.full_messages)
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-    validates :email, :password_digest, :session_token, presence :true
-    validates :password, lenght: {minimum: 6, allow_nil: true}
+    validates :email, :password_digest, :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
     after_initialize :ensure_session_token
 
     attr_reader :password
@@ -27,14 +27,14 @@ class User < ApplicationRecord
     end
 
     def self.generate_session_token 
-        SecureRandom.url_base64(16)
+        SecureRandom.urlsafe_base64(16)
     end
 
     def ensure_session_token
         self.session_token ||= self.class.generate_session_token
     end
 
-    def reset_session_token
+    def reset_session_token!
         self.session_token = self.class.generate_session_token
         self.save
         self.session_token
