ae278d08a44f0ed3d706eb7e320f9ae5b94610b9
Zohaib <khaliqzohaib92@gmail.com>
2020-06-05 02:25:16 -0700

    Merge fix

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-class Api::UsersController < ApplicationController
-
-    def create
-        @user = User.new(user_params)
-        debugger
-        if @user.save
-            signin(@user)
-            render :signup
-        else
-            my_render(422, @user.errors.full_messages)
-        end
-    end
-
-    def user_params
-        params.require(:user).permit(:email, :password)
-    end
-end
-=======
 class Api::UsersController < ApplicationController
 
     def create
@@ -34,4 +15,3 @@ class Api::UsersController < ApplicationController
         params.require(:user).permit(:email, :password)
     end
 end
->>>>>>> auth
