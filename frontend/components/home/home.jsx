10e306c579e9487ad7b9d646e1ecce942db7ccaa
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-05 23:02:00 -0700

    Show json of video and category data receive in home component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+
+    constructor(props){
+        super(props);
+
+    }
+
+    componentDidMount(){
+        this.props.fetchCategories();
+        this.props.fetchVideos();
+    }
+
     render() {
         return (
             <div>
-                HOME
+                <p>Categoreis: {JSON.stringify(this.props.categories)}</p>
+                <br/>
+                <p>Videos: {JSON.stringify(this.props.videos)}</p>
             </div>
         );
     }
--- frontend/components/home/home_container.js
+++ frontend/components/home/home_container.js
@@ -1,16 +1,21 @@
 import {connect} from 'react-redux';
 import Home from './home'
-
+import { CURRENT_PROFILE_ID } from '../../util/constants';
+import {fetchVideos} from '../../actions/video/video_action'
+import {fetchCategories} from '../../actions/category/category_action'
 const mSTP = (state)=>{
     return {
-
+        currentProfileId: state.session[CURRENT_PROFILE_ID],
+        videos: Object.values(state.entities.videos),
+        categories: Object.values(state.entities.categories),
     };
 };
 
 
 const mDTP = (dispatch)=>{
     return{
-
+        fetchVideos:()=>dispatch(fetchVideos()),
+        fetchCategories:()=>dispatch(fetchCategories()),
     };
 };
 
