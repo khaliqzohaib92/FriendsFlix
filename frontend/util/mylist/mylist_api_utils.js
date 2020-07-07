73d692bb89ab88c21007b6a91d273bba25af734b
zohaib <khaliqzohaib92@gmail.com>
2020-07-06 23:34:17 -0400

    Add action for add and fetch mylist

---
+++ frontend/util/mylist/mylist_api_utils.js
@@ -0,0 +1,18 @@
+
+export const addToMyList = (videoId)=>{
+    return $.ajax({
+        url: `api/mylists/${videoId}`
+    })
+};
+
+export const fetchMyList = (profileId)=>{
+    return $.ajax({
+        url: `api/profiles/${profileId}/mylists`
+    })
+};
+
+// export const deleteMyList = (myListId) =>{
+//     return $.ajax({
+//         url: `api/videos/?q=${name.toLowerCase()}`
+//     })
+// }
